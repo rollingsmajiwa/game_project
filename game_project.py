93cd2c61669b48749f89b546cedd4f6df3d0a2d9
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-08-16 15:41:09 +0300

    feat: add feedback

--- game_project.py
+++ game_project.py
@@ -1,11 +1,11 @@
 import random
 
-print("Please select one level: 1. High 2. Medium 3. Low")
+print("Please select one level: 1. Hard 2. Medium 3. Low")
 games = [1, 2, 3]
 attempts = 0
 User_answer = int(input(":"))
 if User_answer == games[0]:
-        user_choice = int(input("Enter your selected number:"))
+        user_choice = int(input("Enter your selected number(1 - 100):"))
         real_answer = random.randint(1, 100)
         maximum_attempt = 5
         is_numrange = range(5)
@@ -14,6 +14,10 @@ if User_answer == games[0]:
                 print("correct")
                 break
             else:
+                if user_choice > real_answer:
+                    print("Too high")
+                else:
+                    print("Too low")
                 Remaining_attempts = maximum_attempt - attempts
                 print(f"Remaining attempts are {Remaining_attempts}. Try again")
                 attempts += 1
@@ -22,7 +26,7 @@ if User_answer == games[0]:
                 print(f"The answer is {real_answer}")
                 break
 elif User_answer == games[1]:
-    user_choice = int(input("Enter your selected number:"))
+    user_choice = int(input("Enter your selected number(1 -100):"))
     real_answer = random.randint(1, 100)
     is_numrange = range(7)
     maximum_attempt = 7
@@ -31,6 +35,10 @@ elif User_answer == games[1]:
             print("correct")
             break
         else:
+            if user_choice > real_answer:
+                print("Too high")
+            else:
+                print("Too low")
             Remaining_attempts = maximum_attempt - attempts
             print(f"Remaining attempts are {Remaining_attempts}. Try again")
             attempts += 1
@@ -39,7 +47,7 @@ elif User_answer == games[1]:
                 print(f"The answer is {real_answer}")
                 break
 elif User_answer == games[2]:
-    user_choice = int(input("Enter your selected number:"))
+    user_choice = int(input("Enter your selected number(1-100):"))
     real_answer = random.randint(1, 100)
     maximum_attempt = 10
     is_numrange = range(10)
@@ -48,6 +56,10 @@ elif User_answer == games[2]:
             print("correct")
             break
         else:
+            if user_choice > real_answer:
+                print("Too high")
+            else:
+                print("Too low")
             Remaining_attempts = maximum_attempt - attempts
             print(f"Remaining attempts are {Remaining_attempts}. Try again")
             attempts += 1
