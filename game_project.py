891d75d0fc86dc2d1659663bfa9ae4f38426194f
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-08-16 14:33:16 +0300

    Feat: add remaining attempts, fix error when calculating remaining attempts

--- game_project.py
+++ game_project.py
@@ -2,18 +2,20 @@ import random
 
 print("Please select one level: 1. High 2. Medium 3. Low")
 games = [1, 2, 3]
+attempts = 0
 User_answer = int(input(":"))
 if User_answer == games[0]:
         user_choice = int(input("Enter your selected number:"))
         real_answer = random.randint(1, 100)
-        attempts = 0
+        maximum_attempt = 5
         is_numrange = range(5)
         for i in is_numrange:
             if user_choice == real_answer:
                 print("correct")
                 break
             else:
-                print("failed")
+                Remaining_attempts = maximum_attempt - attempts
+                print(f"Remaining attempts are {Remaining_attempts}. Try again")
                 attempts += 1
             user_choice = int(input("Try again:"))
             if attempts == 5:
@@ -22,14 +24,15 @@ if User_answer == games[0]:
 elif User_answer == games[1]:
     user_choice = int(input("Enter your selected number:"))
     real_answer = random.randint(1, 100)
-    attempts = 0
     is_numrange = range(7)
+    maximum_attempt = 7
     for i in is_numrange:
         if user_choice == real_answer:
             print("correct")
             break
         else:
-            print("failed")
+            Remaining_attempts = maximum_attempt - attempts
+            print(f"Remaining attempts are {Remaining_attempts}. Try again")
             attempts += 1
             user_choice = int(input("Try again:"))
             if attempts == 7:
@@ -38,14 +41,15 @@ elif User_answer == games[1]:
 elif User_answer == games[2]:
     user_choice = int(input("Enter your selected number:"))
     real_answer = random.randint(1, 100)
-    attempts = 0
+    maximum_attempt = 10
     is_numrange = range(10)
     for i in is_numrange:
         if user_choice == real_answer:
             print("correct")
             break
         else:
-            print("failed")
+            Remaining_attempts = maximum_attempt - attempts
+            print(f"Remaining attempts are {Remaining_attempts}. Try again")
             attempts += 1
             user_choice = int(input("Try again:"))
             if attempts == 10:
