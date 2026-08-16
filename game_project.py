a4796da4bfa5776480bd047e2bf73abc8022c91b
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-08-16 13:53:25 +0300

    Feat: Add user input based on the first level

--- game_project.py
+++ game_project.py
@@ -1,2 +1,20 @@
 print("Please select one level: 1. High 2. Medium 3. Low")
 games = [1, 2, 3]
+User_answer = int(input(":"))
+if User_answer == games[0]:
+        user_choice = int(input("Enter your selected number:"))
+        real_answer = random.randint(1, 100)
+        attempts = 0
+        is_numrange = range(3)
+        for i in is_numrange:
+            if user_choice == real_answer:
+                print("correct")
+                break
+            else:
+                print("failed")
+                attempts += 1
+            user_choice = int(input("Try again:"))
+            if attempts == 3:
+                print(f"The answer is {real_answer}")
+                break
+    
