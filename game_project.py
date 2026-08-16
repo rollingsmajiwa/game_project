76205d06c6f0fee9dcd8aadd7b38119a03eb24f4
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-08-16 13:59:25 +0300

    Feat: Add input for second game and for loop

--- game_project.py
+++ game_project.py
@@ -1,3 +1,5 @@
+import random
+
 print("Please select one level: 1. High 2. Medium 3. Low")
 games = [1, 2, 3]
 User_answer = int(input(":"))
@@ -17,4 +19,19 @@ if User_answer == games[0]:
             if attempts == 3:
                 print(f"The answer is {real_answer}")
                 break
-    
+elif User_answer == games[1]:
+    user_choice = int(input("Enter your selected number:"))
+    real_answer = random.randint(1, 100)
+    attempts = 0
+    is_numrange = range(10)
+    for i in is_numrange:
+        if user_choice == real_answer:
+            print("correct")
+            break
+        else:
+            print("failed")
+            attempts += 1
+            user_choice = int(input("Try again:"))
+            if attempts == 10:
+                print(f"The answer is {real_answer}")
+                break   
